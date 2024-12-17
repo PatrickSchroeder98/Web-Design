5d1f6c7c15a0a9479e29d9dc40fdcfbedd49ffc1
Patrick Schroder <patrickschroder479@gmail.com>
2024-12-17 15:32:05 +0100

    Started work on views
    Added classes in views, initialized templates.

---
+++ blog_project/blog_project/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path(r'post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail')
+]
--- blog_project/blog_project/blog/views.py
+++ blog_project/blog_project/blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView)
+from .models import Post, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(CreateView):
+
+    model = Post
